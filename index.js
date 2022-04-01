f1da2c7d08d59c549b2c098f468a8567b09bb05d
Tanmay Mutalik <tanmaymutalik2002@gmail.com>
2022-04-01 19:59:43 +0530

    Database Server initiated

---
+++ index.js
@@ -0,0 +1,23 @@
+const connectToMongoose = require("./database/db");
+
+// const cors = require("cors");
+const express = require("express");
+
+connectToMongoose();
+
+const app = express();
+const PORT = 5000;
+
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+// };
+
+// app.use(cors(corsOptions));
+
+app.use(express.json());
+
+// app.use("/api/auth", require("./routes/auth.route.js"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
