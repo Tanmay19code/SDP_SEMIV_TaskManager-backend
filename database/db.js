691ddca3f8260abe69737f34d058ffdbc667df38
Tanmay19code <tanmaymutalik2002@gmail.com>
2022-06-09 20:47:47 +0530

    trial refactor

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./.env" });
 
-const mongoURI = process.env.CONNECTION_STRING;
+// const mongoURI = process.env.CONNECTION_STRING;
+const mongoURI = "mongodb+srv://tanmay:tanmay@cluster0.48guw.mongodb.net/test";
 
 const connectToMongoose = () => {
   mongoose.connect(mongoURI, () => {
